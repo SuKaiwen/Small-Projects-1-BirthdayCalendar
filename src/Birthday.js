9b735ae9f36d8b95c65b6734dd87236a87f1d6ee
Kevin Su <kevinsu@outlook.co.nz>
2021-09-01 14:05:22 +1200

    birthday mapping

--- src/Birthday.js
+++ src/Birthday.js
@@ -1,9 +1,54 @@
 import React from 'react';
 
+const dummyData = [
+    {
+      id: 1,
+      name: 'Bertie Yates',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    },
+    {
+      id: 2,
+      name: 'Hester Hogan',
+      age: 32,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+      id: 3,
+      name: 'Larry Little',
+      age: 36,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+      id: 4,
+      name: 'Sean Walsh',
+      age: 34,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+      id: 5,
+      name: 'Lola Gardner',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+];
+
 const Birthday = () => {
     return (
         <>
             <h2>Birthdays here</h2>
+            {dummyData.map((data) =>
+                <div>
+                    <h2>{data.name}</h2>
+                    <h2>{data.age}</h2>
+                    <h2><img src={data.image} alt={data.name} /></h2>
+                </div>
+            )}
         </>
     );
 };
